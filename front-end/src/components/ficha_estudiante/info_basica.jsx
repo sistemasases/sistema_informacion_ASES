a70ac093a837f39125dba8ccf1b7767e86b62b12
Jose David Erazo <jose.david.erazo@correounivalle.edu.co>
2022-10-08 07:00:32 -0500

    cambis pequeños a ficha de estudiante

--- front-end/src/components/ficha_estudiante/info_basica.jsx
+++ front-end/src/components/ficha_estudiante/info_basica.jsx
@@ -5,7 +5,9 @@ import {Container, Row, Col, Dropdown, Button} from "react-bootstrap";
 import {FaRegChartBar, FaThList, FaBars} from "react-icons/fa";
 import {DropdownItem, DropdownToggle, DropdownMenu} from 'reactstrap';
 import { NavLink } from 'react-router-dom';
-import Selector_estudiantes from "../componentes_generales/selector_estudiantes"
+import Selector_estudiantes from "../componentes_generales/selector_estudiantes";
+import  {useEffect} from 'react';
+import axios from 'axios';
 
 
 const Info_basica = () =>{
@@ -13,11 +15,120 @@ const Info_basica = () =>{
     const[switchChecked, setChecked] = useState(false);
     const handleChange = () => setChecked(!switchChecked);
 
-    const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'y', label: 'x' }
-    ]
+
+    const datos_option_user = []
+    const datos_option_rol = []
+    var bandera_option_user = true;
+    var bandera_option_rol = true;
+    var bandera = true;
+    const [state,set_state] = useState({
+      rol: '',
+      usuario : '',
+      data_user : [],
+      data_rol : [],
+    })
+  
+    useEffect(()=>{
+      axios({
+        // Endpoint to send files
+        url:  "http://127.0.0.1:8000/usuario_rol/alluser/",
+        method: "GET",
+      })
+      .then((respuesta)=>{
+        set_state({
+          ...state,
+          data_user : respuesta.data
+        })
+      })
+      .catch(err=>{
+          return (err)
+      })
+      
+    },[]);
+  
+    const aja = (e)=>{
+      if(bandera=true){
+        bandera = false
+        axios({
+          // Endpoint to send files
+          url:  "http://127.0.0.1:8000/usuario_rol/allrol/",
+          method: "GET",
+        })
+        .then((respuesta)=>{
+          set_state({
+            ...state,
+            data_rol : respuesta.data
+          })
+        })
+        .catch(err=>{
+            return (err)
+        })
+  
+      }
+  
+      
+    }
+    const handle_all_rol = (e)=>{
+      if(bandera_option_rol==true){
+  
+        for (var i = 0; i < state.data_rol['length'] ; i++) {
+          const dato2 = { value: state.data_rol[i]['nombre'], label: state.data_rol[i]['nombre'],id:state.data_rol[i]['id'] }
+          datos_option_rol.push(dato2)
+        }
+        console.log([datos_option_rol]);
+        bandera_option_rol = false;
+      }
+      else{
+        console.log([datos_option_rol]);
+      }
+    }
+    
+  
+   
+  
+  
+    const handle_option_user = (e) => {
+      // Getting the files from the input
+      console.log(e)
+      set_state({
+        ...state,
+        usuario : [e.value],
+      })
+    }
+    const handle_option_rol = (e) => {
+      // Getting the files from the input
+      console.log(e)
+      set_state({
+        ...state,
+        rol : [e.value],
+      })
+    }
+    const handle_users = (e) => {
+      // Getting the files from the input
+      if(bandera_option_user==true){
+  
+        for (var i = 0; i < state.data_user['length'] ; i++) {
+          const dato = { value: state.data_user[i]['first_name']+" "+state.data_user[i]['last_name'], label: state.data_user[i]['first_name']+" "+state.data_user[i]['last_name'],id:state.data_user[i]['id'] }
+          datos_option_user.push(dato)
+        }
+        console.log([datos_option_user]);
+        bandera_option_user = false;
+      }
+      else{
+        console.log([datos_option_user]);
+      }
+    }
+    const handle_upload = (e) => {
+      // Getting the files from the input
+      console.log([state.rol])
+      console.log([state.usuario])
+    }
+
+
+
+
+
+
 
     return (
         <Container>
@@ -25,14 +136,16 @@ const Info_basica = () =>{
                 <Row className="row1">
                     <Col className="col1">
                         <Row>
-                            <Select options={options}/>
+                            <Select/>
                         </Row>
 
                         <Row className="rowJustFlex">
                             <Col className="colInfo1">
                                 <Row className="infoRow1">
                                     <Col className="info_basica_selector">
-                                        <Select options={options} className="justMargin1" />
+                                        <Select onMouseEnter ={() => aja} 
+                                        options={datos_option_user} onMenuOpen={handle_users} 
+                                        onChange={handle_option_user}  className="justMargin1" />
                                     </Col>  
                                     <Row className="info"> 
                                         <Col className="info_texto">
@@ -59,7 +172,7 @@ const Info_basica = () =>{
                                     <Col className="inforow23">
                                         <Row className="infoRow23">
                                             <h4>van los cuadros verdes </h4>
-                                            <Select options={options} />
+                                            <Select  />
                                             <Switch onClick={handleChange}/>
                                         </Row>
                                     </Col>
@@ -68,7 +181,7 @@ const Info_basica = () =>{
                             <Col className="colInfo2">
                                 <Row className="infoRow3">
                                     <h4>ICETEX </h4>
-                                    <Select options={options} />
+                                    <Select  />
                                 </Row>
                                 <Row className="infoRow3">
                                     <h4>Cohortes </h4>
