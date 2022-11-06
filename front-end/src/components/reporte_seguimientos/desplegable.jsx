32882ede8c63bb35897589b7e5d0da9755022037
Jose David Erazo <jose.david.erazo@correounivalle.edu.co>
2022-11-06 14:34:18 -0500

    borrar y agregar side

--- front-end/src/components/reporte_seguimientos/desplegable.jsx
+++ front-end/src/components/reporte_seguimientos/desplegable.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {Container, Row, Col, Dropdown, Button} from "react-bootstrap";
 
-import Desplegable_item from "./desplegable_item";
+import Desplegable_item from "./desplegable_Item";
 import items from "./seleccionado.json";
 
 
