0163dbbbbbb18b8b6053c0baf5c65e8bc8462f8b
Dalex11 <54454160+Dalex11@users.noreply.github.com>
2023-03-22 19:32:53 -0500

    Update create_user.js

--- front-end/src/service/create_user.js
+++ front-end/src/service/create_user.js
@@ -5,21 +5,26 @@ const user_rol = (formData) => {
     const url_axios = 'http://localhost:8000/usuario_rol/user/';
 
     var fecha_actual = new Date();
+    var fecha_actual_string = fecha_actual.toISOString();
 
     var usuario_nuevo = {
         "password": formData.password,
-        "last_login": fecha_actual,
+        "last_login": null,
         "is_superuser": false,
         "username": formData.username,
         "first_name": formData.first_name,
         "last_name": formData.last_name,
         "email": formData.email,
         "is_staff": false,
-        "is_active": true,
-        "date_joined": fecha_actual,
+        "is_active": false,
+        "date_joined": fecha_actual_string,
         "groups": [],
         "user_permissions": []
     }
+
+    console.log(fecha_actual)
+    console.log(fecha_actual_string)
+    console.log(usuario_nuevo)
 
     axios.post(url_axios, usuario_nuevo);
 
