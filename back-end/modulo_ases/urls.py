a73f0d3b1cc94f44f9c0bde726e5fe9cf3092264
Steven B. O <stevenbernal2001@gmail.com>
2023-06-22 12:27:36 -0500

    modulo reporte creado

--- back-end/modulo_ases/urls.py
+++ back-end/modulo_ases/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('usuario_rol/', include('modulo_usuario_rol.routers')),
     path('seguimiento/', include('modulo_seguimiento.routers')),
     path('asignacion/', include('modulo_asignacion.routers')),
+    path('reportes/', include('modulo_reportes.routers')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
---
+++ back-end/modulo_reportes/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ModuloReportesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'modulo_reportes'
