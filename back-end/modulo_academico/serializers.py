163e7aea8a24339663b59c62271203dd130fd186
luisgarogu <luis.gabriel.rodriguez@correounivalle.edu.co>
2023-07-27 12:58:04 -0500

    Cambios Seguridad (Serializers)

--- back-end/modulo_academico/serializers.py
+++ back-end/modulo_academico/serializers.py
@@ -21,9 +21,6 @@ from .models import (
 
 
 class historial_academico_serializer(serializers.ModelSerializer):
-    # id_programa_estudiante = programa_estudiante_serializer()
-    # id_semestre = semestre_serializer()
-
     class Meta:
         model = historial_academico
         fields = '__all__'
@@ -41,49 +38,31 @@ class profesor_serializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-
 class matricula_serializer(serializers.ModelSerializer):
-    # id_curso = curso_serializer()
-    # id_estudiante = estudiante_serializer()
-
     class Meta:
         model = matricula
         fields = '__all__'
 
 
 class items_historico_serializer(serializers.ModelSerializer):
-    # id_curso = curso_serializer()
-    # id_semestre = semestre_serializer()
-
     class Meta:
         model = items_historico
         fields = '__all__'
 
 
 class items_semestre_serializer(serializers.ModelSerializer):
-    # id_curso = curso_serializer()
-    # id_semestre = semestre_serializer()
-
     class Meta:
         model = items_semestre
         fields = '__all__'
 
 
 class notas_historico_serializer(serializers.ModelSerializer):
-    # id_item = items_historico_serializer()
-    # id_estudiante = estudiante_serializer()
-
     class Meta:
         model = notas_historico
         fields = '__all__'
 
 
 class notas_semestre_serializer(serializers.ModelSerializer):
-    # id_item = items_semestre_serializer()
-    # id_estudiante = estudiante_serializer()
-
     class Meta:
         model = notas_semestre
         fields = '__all__'
-
-
