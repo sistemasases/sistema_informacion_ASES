e1593c8d735a8975c1e5e2e84364e02add072d15
Dalex11 <54454160+Dalex11@users.noreply.github.com>
2023-08-11 16:28:25 -0500

    Update settings.py

--- back-end/modulo_ases/settings.py
+++ back-end/modulo_ases/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
-SECRET_KEY = 'fu-=7&2jpq2!%j)g1u0ebu-ftobtnpmmi#h4m%%i@*m)(!='
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_INSECURE_SETTINGS') == 'True'
