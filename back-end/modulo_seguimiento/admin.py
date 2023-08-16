76683d5cd53a140cc8acb607815f230ec3938779
César Becerra <sezaru@hotmail.es>
2023-08-16 11:50:15 -0500

    se acomodó el admin de django y se solucionó el error de la tabla de usuarios_rol

--- back-end/modulo_seguimiento/admin.py
+++ back-end/modulo_seguimiento/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from modulo_seguimiento.models import *
 
-# Register your models here.
-admin.site.register(seguimiento_individual)
-admin.site.register(inasistencia)
+class seguimiento_individual_admin(admin.ModelAdmin):
+    list_display = ('id','fecha', 'id_estudiante') 
+
+class inasistencia_admin(admin.ModelAdmin):
+    list_display = ('id','fecha', 'id_estudiante') 
+
+
+admin.site.register(seguimiento_individual,seguimiento_individual_admin)
+admin.site.register(inasistencia,inasistencia_admin)
