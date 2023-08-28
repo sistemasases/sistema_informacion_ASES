17e0d25c95efad8c16c448767848be8c363ca8b5
Dalex11 <54454160+Dalex11@users.noreply.github.com>
2023-08-27 23:51:44 -0500

    inasistencias hechas

--- back-end/modulo_seguimiento/views.py
+++ back-end/modulo_seguimiento/views.py
@@ -43,17 +43,17 @@ class inasistencia_viewsets (viewsets.ModelViewSet):
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response(serializer.data)
+    # def update(self, request, *args, **kwargs):
+    #     partial = kwargs.pop('partial', False)
+    #     instance = self.get_object()
+    #     serializer = self.get_serializer(instance, data=request.data, partial=partial)
+    #     serializer.is_valid(raise_exception=True)
+    #     self.perform_update(serializer)
+    #     return Response(serializer.data)
 
-    def partial_update(self, request, *args, **kwargs):
-        kwargs['partial'] = True
-        return self.update(request, *args, **kwargs)
+    # def partial_update(self, request, *args, **kwargs):
+    #     kwargs['partial'] = True
+    #     return self.update(request, *args, **kwargs)
 
 class seguimientos_estudiante_viewsets (viewsets.ModelViewSet):
     serializer_class = seguimiento_individual_serializer
--- front-end/src/components/seguimiento_forms/form_inasistencia.jsx
+++ front-end/src/components/seguimiento_forms/form_inasistencia.jsx
@@ -1,17 +1,46 @@
 import React, {useState} from 'react';
 import {Container, Row, Col, Dropdown, Button, Modal, ModalHeader, ModalBody, FormCheck} from "react-bootstrap";
 import Form from 'react-bootstrap/Form';
+import Create_Inasistencia from '../../service/create_inasistencia';
 
 
 const Inasistencia = (props) =>{
 
-    const set_info = (e) => {
+    const [state, set_state] = useState({
+        fecha: null,
+        observaciones: "",
+        revisado_profesional: false,
+        revisado_practicante: false,
+        id_creador: parseInt(sessionStorage.getItem("id_usuario")),
+        id_modificador: null,
+        id_estudiante: parseInt(sessionStorage.getItem("id_estudiante_seleccionado"))
+        }
+    )
+
+    const set_info = () => {
+        console.log(state);
+        Create_Inasistencia.create_inasistencia(state).then(res=>{
+            if(res){
+                props.handleCloseIn()
+            } else {
+                window.confirm("Hubo un error al momento de crear el seguimiento, por favor verifique si los datos que ingreso son correctos y que llenó toda la información obligatoria.")
+            }
+        })
     }
 
     const handleChange = () => {
         props.handleCloseIn()
         props.handleModal()
     }
+
+    const handleForm = (e) => {
+        set_state({
+            ...state,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const userRole = sessionStorage.getItem('rol');
 
     return (
         
@@ -29,7 +58,7 @@ const Inasistencia = (props) =>{
                             <h6>Fecha*:</h6>
                         </Row>
                         <Row className="g-2">
-                            <Form.Control type="date" />
+                            <Form.Control type="date" name="fecha" onChange={handleForm}/>
                         </Row>
                     </Col>
                 </Row>
@@ -38,17 +67,9 @@ const Inasistencia = (props) =>{
                     <h6>Observaciones*:</h6>
                 </Row>
                 <Row className="g-2">
-                    <Form.Control as="textarea"  rows={3}/>
+                    <Form.Control as="textarea"  rows={3} name="observaciones" onChange={handleForm}/>
                 </Row>
-                <br/> 
-                <Row>
-                    <Col>
-                        <Form.Check type="checkbox" label="Revisado profesional" />        
-                    </Col>
-                    <Col>
-                        <Form.Check type="checkbox" label="Revisado practicante" />        
-                    </Col>
-                </Row>
+                <br/>
             </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={set_info}>
