3e26532b599eaf8fc30f06491d913d0689d326b2
ASES Dev <sistemas.ases@correounivalle.edu.co>
2023-10-17 11:45:55 -0500

    Pruebas (#46)
    * solucionado error multiples cohortes
    
    * ahora la lista de estudiantes de cada curso se organiza por apellido (#41) (#42)
    
    Co-authored-by: César Becerra <sezaru@hotmail.es>
    
    * Desarrollo (#45)
    
    * ahora la lista de estudiantes de cada curso se organiza por apellido (#41)
    
    Co-authored-by: César Becerra <sezaru@hotmail.es>
    
    * Descripción: (#44)
    
    Se soluciona el error que no permitía filtrar usando las barras de búsqueda del calificador
    
    Cambios realizados:
    - Se corrigió el error de las barras de búsqueda del calificador.
    - Se agregó una barra de búsqueda para los cursos.
    
    Co-authored-by: Carlosmtp <carlos.mauricio.tovar@correounivalle.edu.co>
    
    ---------
    
    Co-authored-by: César Becerra <sezaru@hotmail.es>
    Co-authored-by: Carlosmtp <carlos.mauricio.tovar@correounivalle.edu.co>
    
    ---------
    
    Co-authored-by: César Becerra <sezaru@hotmail.es>
    Co-authored-by: Carlosmtp <carlos.mauricio.tovar@correounivalle.edu.co>

--- front-end/src/components/academico_pestaña/desplegable_Item_listas_materias.jsx
+++ front-end/src/components/academico_pestaña/desplegable_Item_listas_materias.jsx
@@ -1,136 +1,185 @@
-import React, {useMemo, useState} from 'react';
-import {Container, Row, Col, Dropdown, Button} from "react-bootstrap";
+import React, { useMemo, useState } from 'react';
+import { Container, Row, Col, Dropdown, Button } from "react-bootstrap";
 import axios from 'axios';
 import { Link } from "react-router-dom";
-import {decryptTokenFromSessionStorage} from '../../modulos/utilidades_seguridad/utilidades_seguridad.jsx';
+import { decryptTokenFromSessionStorage } from '../../modulos/utilidades_seguridad/utilidades_seguridad.jsx';
 
-const Desplegable_item_listas_materias = ({item, franja}) => {
+const Desplegable_item_listas_materias = ({ item, franja }) => {
 
-    const config = {
-        headers: {
-            Authorization: 'Bearer ' + decryptTokenFromSessionStorage(),
-        }
-      };
+  const config = {
+    headers: {
+      Authorization: 'Bearer ' + decryptTokenFromSessionStorage(),
+    }
+  };
 
-    const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false)
 
-    const [state,set_state] = useState({
-        cursos_de_la_facultad : [],
-        franjas_de_curso : [],
-        profesores_de_la_franja : [],
-        alumnos_del_profesor : []
+  const [state, set_state] = useState({
+    filtro: '',
+    cursos_de_la_facultad: [],
+    franjas_de_curso: [],
+    profesores_de_la_franja: [],
+    alumnos_del_profesor: []
 
+  })
+
+  const traer_cursos_de_facultad = async (index) => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}/academico/cursos_facultad/` + index + "/", config);
+      set_state({
+        cursos_de_la_facultad: response.data
       })
-    
-      const traer_cursos_de_facultad = async (index)=>{
-        try{
-          const response = await axios.get(`${process.env.REACT_APP_API_URL}/academico/cursos_facultad/` + index + "/", config);
-          set_state({
-            cursos_de_la_facultad : response.data
-          })
-        }
-        catch (error){
-          console.log("no capto el dato")
-        }
-      }
+    }
+    catch (error) {
+      console.log("no capto el dato")
+    }
+  }
 
 
-      const franjas_del_curso = async (index)=>{
-        try{
-          const response = await axios.get(`${process.env.REACT_APP_API_URL}/academico/franja_curso/` + index + "/", config);
-          set_state({
-            franjas_de_curso : response.data
-          })
-        }
-        catch (error){
-          console.log("no capto el dato")
-        }
-      }
+  const franjas_del_curso = async (index) => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}/academico/franja_curso/` + index + "/", config);
+      set_state({
+        franjas_de_curso: response.data
+      })
+    }
+    catch (error) {
+      console.log("no capto el dato")
+    }
+  }
 
-      const profesores_de_la_franja = async (index, index2)=>{
-        try{
-          const response = await axios.get(`${process.env.REACT_APP_API_URL}/academico/profesores_del_curso/`,
-                                    {params : {curso : index, franja : index2}}, config);
-          set_state({
-            profesores_de_la_franja : response.data
-          })
-        }
-        catch (error){
-          console.log("no capto el dato")
-        }
-      }
+  const profesores_de_la_franja = async (index, index2) => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}/academico/profesores_del_curso/`,
+        { params: { curso: index, franja: index2 } }, config);
+      set_state({
+        profesores_de_la_franja: response.data
+      })
+    }
+    catch (error) {
+      console.log("no capto el dato")
+    }
+  }
 
-      const alumnos_del_profesor = async (index, index2)=>{
-        try{
-          const response = await axios.get(`${process.env.REACT_APP_API_URL}/academico/alumnos_del_profesor/`, 
-                                                        {params : {curso : index, profesor : index2}}, config);
-          set_state({
-            alumnos_del_profesor : response.data
-          })
-        }
-        catch (error){
-          console.log("no capto el dato")
-        }
-      }
+  const alumnos_del_profesor = async (index, index2) => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}/academico/alumnos_del_profesor/`,
+        { params: { curso: index, profesor: index2 } }, config);
+      set_state({
+        alumnos_del_profesor: response.data
+      })
+    }
+    catch (error) {
+      console.log("no capto el dato")
+    }
+  }
+  const cambiar_dato = (e) => {
+    set_state({
+      ...state,
+      [e.target.name]: e.target.value
+    })
+  }
 
 
-    /*if(item.codigo_univalle){
-        return (
-            <Row>
-                <Col className={open ? "fichas_academico open" : "fichas_academico"}>
-                    <Row className="link_academico1" onClick={() => {setOpen(!open); traer_cursos_de_facultad(item.id)}}>
+  /*if(item.codigo_univalle){
+      return (
+          <Row>
+              <Col className={open ? "fichas_academico open" : "fichas_academico"}>
+                  <Row className="link_academico1" onClick={() => {setOpen(!open); traer_cursos_de_facultad(item.id)}}>
+                      <Col className="link_text_academico1" >
+                          <Row className="link_text_academico_hover1">
+                              {item.nombre}
+                          </Row>
+                      </Col>
+                  </Row>
+                  <Row className="content_academico">
+                      
+                      <Col className="contenido_fichas_academico1">
+                          { state.cursos_de_la_facultad.map((child, index) => <Desplegable_item_listas_materias key={index} item={child} />) }
+                      </Col>
+                  </Row>
+              </Col>
+          </Row>
+      )
+  }else */
+
+  if (item.materias) {
+    return (
+      <Row>
+                <Col className={open ? "fichas_academico1 open" : "fichas_academico1"}>
+                    <Row className="link_academico1">
                         <Col className="link_text_academico1" >
-                            <Row className="link_text_academico_hover1">
+                            <Row className="link_text_academico_hover2">
                                 {item.nombre}
+                                Buscar
+                                <Col xs={"12"} md={"2"}>
+                                    <input name="filtro" onChange={cambiar_dato}></input>
+                                </Col>
                             </Row>
                         </Col>
                     </Row>
-                    <Row className="content_academico">
-                        
-                        <Col className="contenido_fichas_academico1">
-                            { state.cursos_de_la_facultad.map((child, index) => <Desplegable_item_listas_materias key={index} item={child} />) }
+
+                    <Row>
+                        <Col className="contenido_fichas_academico2">
+                        { item.materias.filter((item)=>{
+                          console.log(state.filtro)
+                                return state.filtro === '' ? item 
+                                : 
+                                item.nombre.toLowerCase().includes(state.filtro.toLowerCase()) ||
+                                item.cod_materia.toLowerCase().includes(state.filtro.toLowerCase())                                     
+                                }).map((item, index) => <Desplegable_item_listas_materias 
+                                key={index} item={item} 
+                            />) }
+                            
                         </Col>
                     </Row>
                 </Col>
             </Row>
-        )
-    }else */if(item.tipo_dato === 'curso') {
-        return (
-            <Row>
-                <Col className={open ? "fichas_academico2 open" : "fichas_academico2"}>
-                    <Row className="link_academico1" onClick={() => {setOpen(!open); franjas_del_curso(item.cod_materia)}}>
-                        <Col className="link_text_academico1" >
-                            <Row className="link_text_academico_hover2">
-                                {item.nombre}     --     {item.cod_materia}
-                            </Row> 
-                        </Col>
-                    </Row>
-                    <Row className="content_academico">
-                        <Col className="contenido_fichas_academico3">
-                            { state.franjas_de_curso.map((child, index) => 
-                                <Desplegable_item_listas_materias key={index} item={child} franja={item.franja}/>) 
-                            }
-                        </Col>
-                    </Row>
-                </Col>
+    )
+  }
+  else if (item.tipo_dato === 'curso') {
+    return (
+      <>
+        <Row>
+
+          <Col className={open ? "fichas_academico2 open" : "fichas_academico2"}>
+
+
+            <Row className="link_academico1" onClick={() => { setOpen(!open); franjas_del_curso(item.cod_materia) }}>
+              <Col className="link_text_academico1" >
+
+                <Row className="link_text_academico_hover2">
+                  {item.nombre}     --     {item.cod_materia}
+                </Row>
+              </Col>
             </Row>
-        )
-    }
-    else if(item.tipo_dato === 'franja') {
-        return (
-            <Row>
+            <Row className="content_academico">
+              <Col className="contenido_fichas_academico3">
+                {state.franjas_de_curso.map((child, index) =>
+                  <Desplegable_item_listas_materias key={index} item={child} franja={item.franja} />)
+                }
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </>
+    )
+  }
+  else if (item.tipo_dato === 'franja') {
+    return (
+      <Row>
 
-                <Col className={open ? "fichas_academico4 open" : "fichas_academico4"}>
-                    <Row className="link_text_academico_hover4" >
-                        <a href={`/calificador/${encodeURIComponent(item.id)}/${encodeURIComponent(item.id_profesor)}/${encodeURIComponent(item.cod_materia)}/${encodeURIComponent(item.franja)}`} 
-                        rel="noopener noreferrer" className="link_text_academico_hover4">
-                            Grupo {item.franja}     :     {item.profesor_data.first_name}   {item.profesor_data.last_name} 
-                        </a>
-                    </Row>
+        <Col className={open ? "fichas_academico4 open" : "fichas_academico4"}>
+          <Row className="link_text_academico_hover4" >
+            <a href={`/calificador/${encodeURIComponent(item.id)}/${encodeURIComponent(item.id_profesor)}/${encodeURIComponent(item.cod_materia)}/${encodeURIComponent(item.franja)}`}
+              rel="noopener noreferrer" className="link_text_academico_hover4">
+              Grupo {item.franja}     :     {item.profesor_data.first_name}   {item.profesor_data.last_name}
+            </a>
+          </Row>
 
 
 
-                    {/*
+          {/*
                     <Row className="content_academico">
                             <Col className="contenido_fichas_academico2">
                                 { state.profesores_de_la_franja.map((child, index) => 
@@ -139,70 +188,70 @@ const Desplegable_item_listas_materias = ({item, franja}) => {
                             </Col>
                         </Row>
                     */}
-                </Col>
-            </Row>
-        )
-    }
-    
-    else if (item.tipo_dato === 'profesor'){
-        return (
-        <Row>
-        <Col className={open ? "fichas_academico4 open" : "fichas_academico4"}>
-            <Row className="link_academico1_sin_borde" >
-                <Col className="contenido_fichas_academico2" >
-                        <a href={`/calificador/${encodeURIComponent(franja)}/${encodeURIComponent(item.id)}/${encodeURIComponent(item.cod_materia)}/${encodeURIComponent(item.franja)}`} 
-                        rel="noopener noreferrer" className="link_text_academico_hover4">
-                            {item.first_name} {item.last_name}  
-                        </a>
-                </Col>
-            </Row>
-
-            <Row className="content_academico">
-                <Col className="contenido_fichas_academico2" xs={4}>
-                </Col>
-                { item.items_materia.map((item, index) => 
-                    <Col>( {item.nombre} )</Col>)
-                }
-            </Row>
-
-            <Row className="content_academico">
-                <Col className="contenido_fichas_academico3">
-                    {state.alumnos_del_profesor.map((child, index) => <Desplegable_item_listas_materias key={index} item={child}/>) }
-                </Col>
-            </Row>
         </Col>
-    </Row>
-        )
-    }
-    else if (item.tipo_dato === 'estudiante') {
-      return (
-        <Row>
-          <Col className={open ? "fichas_academico4 open" : "fichas_academico4"}>
-            <Row className="link_academico1_sin_borde" onClick={() => setOpen(!open)}>
-              <Col className="link_text_academico1_sin_borde" xs={4}>
-                <Link to={`/ficha_estudiante/${item.id}`} className="fichas_academico plain">
-                  {item.nombre} {item.apellido} - {item.cod_univalle}
-                </Link>
-              </Col>
-              {item.notas ? (
-                item.notas.length > 0 ? ( // Verificar si el array de notas no está vacío
-                  item.notas.map((nota, index) => ( // Si no está vacío, realizar el mapeo
-                    <Col key={index}>( {nota.nombre} : {nota.calificacion} )</Col>
-                  ))
-                ) : (
-                  <Col>No hay notas disponibles</Col> // Si está vacío, mostrar un mensaje o contenido alternativo
-                )
-              ) : (
-                <Col>No hay notas disponibles</Col> // Si item.notas no existe, mostrar un mensaje o contenido alternativo
-              )}
-            </Row>
-          </Col>
-        </Row>
-      );
-    }
-    // ...
+      </Row>
+    )
+  }
 
-    
+  else if (item.tipo_dato === 'profesor') {
+    return (
+      <Row>
+        <Col className={open ? "fichas_academico4 open" : "fichas_academico4"}>
+          <Row className="link_academico1_sin_borde" >
+            <Col className="contenido_fichas_academico2" >
+              <a href={`/calificador/${encodeURIComponent(franja)}/${encodeURIComponent(item.id)}/${encodeURIComponent(item.cod_materia)}/${encodeURIComponent(item.franja)}`}
+                rel="noopener noreferrer" className="link_text_academico_hover4">
+                {item.first_name} {item.last_name}
+              </a>
+            </Col>
+          </Row>
+
+          <Row className="content_academico">
+            <Col className="contenido_fichas_academico2" xs={4}>
+            </Col>
+            {item.items_materia.map((item, index) =>
+              <Col>( {item.nombre} )</Col>)
+            }
+          </Row>
+
+          <Row className="content_academico">
+            <Col className="contenido_fichas_academico3">
+              {state.alumnos_del_profesor.map((child, index) => <Desplegable_item_listas_materias key={index} item={child} />)}
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    )
+  }
+  else if (item.tipo_dato === 'estudiante') {
+    return (
+      <Row>
+        <Col className={open ? "fichas_academico4 open" : "fichas_academico4"}>
+          <Row className="link_academico1_sin_borde" onClick={() => setOpen(!open)}>
+            <Col className="link_text_academico1_sin_borde" xs={4}>
+              <Link to={`/ficha_estudiante/${item.id}`} className="fichas_academico plain">
+                {item.nombre} {item.apellido} - {item.cod_univalle}
+              </Link>
+            </Col>
+            {item.notas ? (
+              item.notas.length > 0 ? ( // Verificar si el array de notas no está vacío
+                item.notas.map((nota, index) => ( // Si no está vacío, realizar el mapeo
+                  <Col key={index}>( {nota.nombre} : {nota.calificacion} )</Col>
+                ))
+              ) : (
+                <Col>No hay notas disponibles</Col> // Si está vacío, mostrar un mensaje o contenido alternativo
+              )
+            ) : (
+              <Col>No hay notas disponibles</Col> // Si item.notas no existe, mostrar un mensaje o contenido alternativo
+            )}
+          </Row>
+        </Col>
+      </Row>
+    );
+  }
+  // ...
+
+
 }
 
 export default Desplegable_item_listas_materias
