2bef15480d571accdef04ce9783b3034c3b2c13d
Dalex11 <54454160+Dalex11@users.noreply.github.com>
2023-11-14 11:04:13 -0500

    Descarga y vista de seguimientos

--- back-end/modulo_seguimiento/views.py
+++ back-end/modulo_seguimiento/views.py
@@ -10,6 +10,12 @@ from modulo_instancia.models import *
 from modulo_instancia.serializers import semestre_serializer
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from django.db.models import Q
+from operator import and_
+from functools import reduce
+from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
+from django.views import View
 
 # Create your views here.
 
@@ -169,65 +175,60 @@ class conteo_seguimientos_estudiante_viewsets (viewsets.ModelViewSet):
 
 
 
+class descarga_seguimientos_inasistencias_viewsets (viewsets.ModelViewSet):
 
+    serializer_class = seguimiento_individual_serializer
+    permission_classes = (IsAuthenticated,)
+    queryset = seguimiento_individual_serializer.Meta.model.objects.all()
 
+    def retrieve(self, request, pk=None):
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print(self.request.data)
+        # print("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        # print(self.request.data["id_estudiante"])
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print(request.data)
+        # print("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        # print(request.data["id_estudiante"])
+        # print("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        list_inasistencia = []
+        list_seguimientos = []
+        list_final =[]
+        list_seguimientos_individual = list(seguimiento_individual.objects.filter(id_estudiante = 1))
+        list_inasistencia_individual = list(inasistencia.objects.filter(id_estudiante = 1))
 
+        for i in list_inasistencia_individual: 
+            serializer_inasistencia = inasistencia_serializer(i)
+            list_inasistencia.append(serializer_inasistencia.data)
+        for i in list_seguimientos_individual: 
+            serializer_seguimiento_individual = seguimiento_individual_serializer(i)
+            list_seguimientos.append(serializer_seguimiento_individual.data)
 
-# class conteo_seguimientos_estudiante_viewsets(viewsets.ModelViewSet):
-#     serializer_class = seguimiento_individual_serializer
-#     queryset = seguimiento_individual_serializer.Meta.model.objects.all()
+        list_semestre = list(semestre.objects.all().filter(semestre_actual = True))
 
-#     def retrieve(self, request, pk=None):
-#         list_inasistencia = list(inasistencia.objects.filter(id_estudiante=pk))
-#         list_seguimientos_individual = list(seguimiento_individual.objects.filter(id_estudiante=pk))
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print(semestre_serializer(semestre.objects.get(semestre_actual = True)))
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
-#         count_inasistencias = len(list_inasistencia)
-#         count_seguimientos = len(list_seguimientos_individual)
+        for i in list_semestre:
+            lista_inasistencia = []
 
-#         count_inasistencias_revisados_practicante = len([s for s in list_inasistencia if s.revisado_practicante])
-#         count_inasistencias_revisados_profesional = len([s for s in list_inasistencia if s.revisado_profesional])
+            lista_seguimientos = []
 
-#         count_seguimientos_revisados_practicante = len([s for s in list_seguimientos_individual if s.revisado_practicante])
-#         count_seguimientos_revisados_profesional = len([s for s in list_seguimientos_individual if s.revisado_profesional])
+            serializer_semestre =semestre_serializer(i)
 
-#         counts = {
-#             'count_inasistencias': count_inasistencias,
-#             'count_seguimientos': count_seguimientos,
-#             'count_inasistencias_revisados_practicante': count_inasistencias_revisados_practicante,
-#             'count_inasistencias_revisados_profesional': count_inasistencias_revisados_profesional,
-#             'count_seguimientos_revisados_practicante': count_seguimientos_revisados_practicante,
-#             'count_seguimientos_revisados_profesional': count_seguimientos_revisados_profesional,
-#         }
+            for j in list_inasistencia:
+                if j['fecha'] > serializer_semestre.data['fecha_inicio'] and j['fecha'] < serializer_semestre.data['fecha_fin']:
+                    lista_inasistencia.append(j)
 
-#         return Response(counts, status=status.HTTP_200_OK)
+            for j in list_seguimientos:
+                if j['fecha'] > serializer_semestre.data['fecha_inicio'] and j['fecha'] < serializer_semestre.data['fecha_fin']:
+                    lista_seguimientos.append(j)
 
+            list_final.append({
+                'semestre': serializer_semestre.data['nombre'],
+                'inasistencias': lista_inasistencia,
+                'seguimientos': lista_seguimientos,
+            })
 
-
-
-# class conteo_seguimientos_estudiante_viewsets(viewsets.ModelViewSet):
-#     serializer_class = seguimiento_individual_serializer
-#     queryset = seguimiento_individual_serializer.Meta.model.objects.all()
-
-#     @action(detail=False, methods=['get'])
-#     def counts(self, request):
-#         pk = request.query_params.get('pk')
-#         if not pk:
-#             return Response({'error': 'Missing student ID'}, status=status.HTTP_400_BAD_REQUEST)
-
-#         list_inasistencia = list(inasistencia.objects.filter(id_estudiante=pk))
-#         list_seguimientos_individual = list(seguimiento_individual.objects.filter(id_estudiante=pk))
-
-#         count_inasistencias = len(list_inasistencia)
-#         count_seguimientos = len(list_seguimientos_individual)
-
-#         count_revisados_practicante = len([s for s in list_seguimientos_individual if s.revisado_practicante])
-#         count_revisados_profesional = len([s for s in list_seguimientos_individual if s.revisado_profesional])
-
-#         counts = {
-#             'count_inasistencias': count_inasistencias,
-#             'count_seguimientos': count_seguimientos,
-#             'count_revisados_practicante': count_revisados_practicante,
-#             'count_revisados_profesional': count_revisados_profesional
-#         }
-
-#         return Response(counts, status=status.HTTP_200_OK)
+        return Response(list_final,status=status.HTTP_200_OK)
