18684fb678d00656f45045bd0f6d085e37708365
ASES Dev <sistemas.ases@correounivalle.edu.co>
2023-11-14 10:21:58 -0500

    Se creó la tabla donde se guardarán los riesgos y se enlazó con la de seguimientos (#81)
    Co-authored-by: Dalex11 <54454160+Dalex11@users.noreply.github.com>

--- back-end/modulo_seguimiento/routers.py
+++ back-end/modulo_seguimiento/routers.py
@@ -6,6 +6,7 @@ router.register(r'seguimiento_individual',seguimiento_individual_viewsets,basena
 router.register(r'inasistencia',inasistencia_viewsets,basename = 'inasistencia')
 router.register(r'seguimientos_estudiante',seguimientos_estudiante_viewsets,basename = 'seguimientos_estudiante')
 router.register(r'seguimientos_estudiante_solo_semestre_actual',seguimientos_estudiante_solo_semestre_actual_viewsets,basename = 'seguimientos_estudiante_solo_semestre_actual')
+router.register(r'riesgo_individual',riesgo_individual_viewsets,basename = 'riesgo_individual')
 
 seguimientos_estudiante_viewsets
 
