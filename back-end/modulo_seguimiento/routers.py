de0e093765b5496732e1930d13e383f830108fd8
Dalex11 <54454160+Dalex11@users.noreply.github.com>
2023-12-02 12:34:49 -0500

    Creación del back

--- back-end/modulo_seguimiento/routers.py
+++ back-end/modulo_seguimiento/routers.py
@@ -7,7 +7,7 @@ router.register(r'inasistencia',inasistencia_viewsets,basename = 'inasistencia')
 router.register(r'seguimientos_estudiante',seguimientos_estudiante_viewsets,basename = 'seguimientos_estudiante')
 router.register(r'seguimientos_estudiante_solo_semestre_actual',seguimientos_estudiante_solo_semestre_actual_viewsets,basename = 'seguimientos_estudiante_solo_semestre_actual')
 router.register(r'conteo_seguimientos_estudiante',conteo_seguimientos_estudiante_viewsets,basename = 'conteo_seguimientos_estudiante')
-router.register(r'descarga_seguimientos_inasistencias',descarga_seguimientos_inasistencias_viewsets,basename = 'descarga_seguimientos_inasistencias')
+router.register(r'seguimientos_individual',descarga_seguimientos_inasistencias_viewsets,basename = 'descarga_seguimientos_inasistencias')
 
 seguimientos_estudiante_viewsets
 
--- back-end/modulo_seguimiento/views.py
+++ back-end/modulo_seguimiento/views.py
@@ -4,7 +4,9 @@ from rest_framework import status
 from rest_framework import viewsets
 from modulo_seguimiento.serializers import *
 from modulo_usuario_rol.serializers import  user_serializer,estudiante_serializer
-from modulo_usuario_rol.models import rol, usuario_rol, estudiante
+from modulo_usuario_rol.models import rol, usuario_rol, estudiante, cohorte_estudiante
+from modulo_programa.models import programa_estudiante, programa
+from modulo_programa.serializers import *
 from modulo_seguimiento.models import *
 from modulo_instancia.models import *
 from modulo_instancia.serializers import semestre_serializer
@@ -181,54 +183,63 @@ class descarga_seguimientos_inasistencias_viewsets (viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
     queryset = seguimiento_individual_serializer.Meta.model.objects.all()
 
-    def retrieve(self, request, pk=None):
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        print(self.request.data)
-        # print("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        # print(self.request.data["id_estudiante"])
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        print(request.data)
-        # print("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        # print(request.data["id_estudiante"])
-        # print("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        list_inasistencia = []
-        list_seguimientos = []
-        list_final =[]
-        list_seguimientos_individual = list(seguimiento_individual.objects.filter(id_estudiante = 1))
-        list_inasistencia_individual = list(inasistencia.objects.filter(id_estudiante = 1))
+    def post(self, request, pk=None):
 
-        for i in list_inasistencia_individual: 
-            serializer_inasistencia = inasistencia_serializer(i)
-            list_inasistencia.append(serializer_inasistencia.data)
-        for i in list_seguimientos_individual: 
-            serializer_seguimiento_individual = seguimiento_individual_serializer(i)
-            list_seguimientos.append(serializer_seguimiento_individual.data)
+        filters_and = []
+        try:
+            if request.data['estudiante']:
+                estudiante_obj = estudiante.objects.get(cod_univalle=request.data['estudiante'])
+                filters_and.append(Q(**{ "id_estudiante": estudiante_obj }))
+            else:
+                estudiante_obj = estudiante.objects.all()
+                filters_and.append(Q(**{ "id_estudiante__in": estudiante_obj }))
+        except:
+                estudiante_obj = estudiante.objects.all()
+                filters_and.append(Q(**{ "id_estudiante__in": estudiante_obj }))
+        try:
+            if request.data['fecha_inicio']:
+                filters_and.append(Q(**{ "fecha__gte": request.data['fecha_inicio'] }))
+        except:
+            pass
+        try:
+            if request.data['fecha_fin']:
+                filters_and.append(Q(**{ "fecha__lte": request.data['fecha_fin'] }))
+        except:
+            pass
+        try:
+            if request.data['programa']:
+                programa_obj = programa.objects.filter(codigo_univalle=request.data['programa'])
+                estudiantes_del_programa = programa_estudiante.objects.filter(id_programa__in=programa_obj).values_list('id_estudiante', flat=True)
+                filters_and.append(Q(**{ "id_estudiante__in": estudiantes_del_programa }))
+        except:
+            pass
+        try:
+            if request.data['sede']:
+                sede_obj = sede.objects.filter(nombre=request.data['sede'])
+                programa_obj = programa.objects.filter(id_sede__in=sede_obj)
+                estudiantes_del_programa = programa_estudiante.objects.filter(id_programa__in=programa_obj).values_list('id_estudiante', flat=True)
+                filters_and.append(Q(**{ "id_estudiante__in": estudiantes_del_programa }))
+        except:
+            pass
+        try:
+            if request.data['cohorte']:
+                cohorte_obj = cohorte.objects.filter(id_number=request.data['cohorte'])
+                estudiantes_del_programa = cohorte_estudiante.objects.filter(id_cohorte__in=cohorte_obj).values_list('id_estudiante', flat=True)
+                filters_and.append(Q(**{ "id_estudiante__in": estudiantes_del_programa }))
+        except:
+            pass
 
-        list_semestre = list(semestre.objects.all().filter(semestre_actual = True))
+        reduce_and = reduce(and_, filters_and)
+        filters = []
+        filters.append(reduce_and)
 
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        print(semestre_serializer(semestre.objects.get(semestre_actual = True)))
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        seguimientos =  seguimiento_individual_serializer(
+                            seguimiento_individual.objects.filter(*filters).order_by("fecha"),
+                            many=True
+                        ).data
+        inasistencias = inasistencia_serializer(
+                            inasistencia.objects.filter(*filters).order_by("fecha"),
+                            many=True
+                        ).data
 
-        for i in list_semestre:
-            lista_inasistencia = []
-
-            lista_seguimientos = []
-
-            serializer_semestre =semestre_serializer(i)
-
-            for j in list_inasistencia:
-                if j['fecha'] > serializer_semestre.data['fecha_inicio'] and j['fecha'] < serializer_semestre.data['fecha_fin']:
-                    lista_inasistencia.append(j)
-
-            for j in list_seguimientos:
-                if j['fecha'] > serializer_semestre.data['fecha_inicio'] and j['fecha'] < serializer_semestre.data['fecha_fin']:
-                    lista_seguimientos.append(j)
-
-            list_final.append({
-                'semestre': serializer_semestre.data['nombre'],
-                'inasistencias': lista_inasistencia,
-                'seguimientos': lista_seguimientos,
-            })
-
-        return Response(list_final,status=status.HTTP_200_OK)
+        return Response({"seguimientos": seguimientos, "inasistencias": inasistencias},status=status.HTTP_200_OK)
