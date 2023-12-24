f2075daa02d737525c28a8139e128cdfdade2ec8
JabCol <nicolvaleria0919@gmail.com>
2023-12-23 20:10:10 -0500

    Se guarda al usuario logueado es una varible global

---
+++ front-end/src/components/ficha_estudiante_dicapacidad/Componentes/AccesoDenegado.jsx
@@ -0,0 +1,13 @@
+import "../../../Scss/ficha_estudiante_discapacidad/acceso_denegado.css"
+
+const AccesoDenegado = () => {
+    return (
+        <div className="container_denegado">
+            <h1>Acceso denegado</h1>
+            <p>No tienes los permisos necesarios para ver esta página.</p>
+            <p>Por favor, contacta al administrador del sistema para obtener acceso.</p>
+        </div>
+    )
+}
+
+export default AccesoDenegado
--- front-end/src/modulos/discapacidad/ficha_estudiante/FichaEstudianteDiscapacidad.jsx
+++ front-end/src/modulos/discapacidad/ficha_estudiante/FichaEstudianteDiscapacidad.jsx
@@ -1,30 +1,49 @@
-import React, { useState } from "react";
-import Acceso_denegado from "../../../components/componentes_generales/acceso_denegado";
+import React, { useEffect } from "react";
 import {
   desencriptar,
-  decryptTokenFromSessionStorage,
   desencriptarInt,
-  decryptUserIdFromSessionStorage,
   desencriptarBigInt,
 } from "../../utilidades_seguridad/utilidades_seguridad.jsx";
 import Select from "../../../components/ficha_estudiante_dicapacidad/Componentes/Select.jsx";
 import Riesgos from "../../../components/ficha_estudiante_dicapacidad/Componentes/Riesgos.jsx";
 import Acordiones from "../../../components/ficha_estudiante_dicapacidad/Componentes/Acordiones.jsx";
 import "../../../Scss/ficha_estudiante_discapacidad/discapacidad.css";
+import { useAuthStore } from "../../../components/ficha_estudiante_dicapacidad/store/auth.js";
+import AccesoDenegado from "../../../components/ficha_estudiante_dicapacidad/Componentes/AccesoDenegado.jsx";
 
 const FichaEstudianteDiscapacidad = (props) => {
+  const { user, setUser } = useAuthStore();
+
+  useEffect(() => {
+    setUser({
+      rol: desencriptar(sessionStorage.getItem("rol")).toString(),
+      sede_id: desencriptarInt(sessionStorage.getItem("sede_id")).toString(),
+      id_usuario: desencriptarBigInt(
+        sessionStorage.getItem("id_usuario")
+      ).toString(),
+      userRole: desencriptar(sessionStorage.getItem("permisos")).toString(),
+    });
+    console.log("USER: ", user);
+  }, []);
+
   return (
-    <div className="container-ficha">
-      <div className="select-container">
-        <Select />
-      </div>
-      <div className="riesgo-container">
-        <Riesgos />
-      </div>
-      <div className="acordion-container">
-        <Acordiones />
-      </div>
-    </div>
+    <>
+      {user && user.userRole.includes("view_ficha_estudiantes") ? (
+        <div className="container-ficha">
+          <div className="select-container">
+            <Select />
+          </div>
+          <div className="riesgo-container">
+            <Riesgos />
+          </div>
+          <div className="acordion-container">
+            <Acordiones />
+          </div>
+        </div>
+      ) : (
+        <AccesoDenegado />
+      )}
+    </>
   );
 };
 
