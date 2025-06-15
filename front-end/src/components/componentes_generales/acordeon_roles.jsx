4fe882c7f59d58a798fe9f3bf8ab1a4267f66f44
Steven B. O. <stevenbernal2001@gmail.com>
2025-06-14 19:22:09 -0500

    panel admin roles - read

--- front-end/src/components/componentes_generales/acordeon_roles.jsx
+++ front-end/src/components/componentes_generales/acordeon_roles.jsx
@@ -8,8 +8,9 @@ import React, { useState, useEffect } from "react";
 import { Container, Accordion } from "react-bootstrap";
 import DataTable from "react-data-table-component";
 import DataTableExtensions from "react-data-table-component-extensions";
-import all_rols_service from "../../service/all_rols";
+// import all_rols_service from "../../service/all_rols";
 import { decryptTokenFromSessionStorage } from "../../modulos/utilidades_seguridad/utilidades_seguridad";
+import Read_roles from "../../service/panel_admin/panel_admin_roles_listar_roles.js";
 
 const SelectorRoles = () => {
   const [state, setState] = useState({
@@ -24,7 +25,8 @@ const SelectorRoles = () => {
 
   const consultaAllRoles = async () => {
     try {
-      const response = await all_rols_service.all_rols();
+      // const response = await all_rols_service.all_rols();
+      const response = await Read_roles.listar_roles({});
       if (response && Array.isArray(response)) {
         setState({ ...state, data_roles: response });
       }
@@ -35,9 +37,27 @@ const SelectorRoles = () => {
 
   // Definición de las columnas de la tabla
   const columnas = [
-    { name: "NOMBRE", selector: (row) => row.nombre, sortable: true },
-    { name: "DESCRIPCIÓN", selector: (row) => row.descripcion, sortable: false },
-    {name: "PERMISOS", selector: (row) => row.permisos?.join(", ") || "Sin permisos", sortable: false},
+    {
+      name: "NOMBRE",
+      selector: (row) => row.nombre,
+      sortable: true,
+      wrap: true,
+      grow: 0.2,
+    },
+    {
+      name: "DESCRIPCIÓN",
+      selector: (row) => row.descripcion,
+      sortable: false,
+      wrap: true,
+      grow: 0.4,
+    },
+    {
+      name: "PERMISOS",
+      selector: (row) => row.permisos?.join(", ") || "Sin permisos",
+      sortable: false,
+      wrap: true,
+      // grow: 0.5,
+    },
   ];
 
   useEffect(() => {
@@ -48,9 +68,7 @@ const SelectorRoles = () => {
     <Container>
       <Accordion>
         <Accordion.Item eventKey="2">
-          <Accordion.Header onClick={consultaAllRoles}>
-            Roles
-          </Accordion.Header>
+          <Accordion.Header onClick={consultaAllRoles}>Roles</Accordion.Header>
           <Accordion.Body>
             <DataTableExtensions columns={columnas} data={state.data_roles}>
               <DataTable
